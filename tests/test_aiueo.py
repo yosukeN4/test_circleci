d9d0c8a052a8dc100564aa866913146b3b276c95
yosukeN <yousken72@gmail.com>
2022-12-08 20:33:20 +0900

    add tests files and folder. fix config.yml

---
+++ tests/test_aiueo.py
@@ -0,0 +1,14 @@
+import unittest
+
+
+
+# class TestHoge(unittest.TestCase):
+#     def test_aiueo():
+#         pass
+
+
+def test_func1():
+    assert 1 == 1
+
+def test_func2():
+    assert 2 == 2
